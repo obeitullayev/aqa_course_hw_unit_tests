a2072b926b9e269fef6f89dba39403ae4fa07f4a
obeitullaev <beyocman@gmail.com>
2025-08-02 16:37:37 +0300

    added HW-4 task-1-3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,27 @@
   преобразовываясь в number
 
 */
+const minAge=18;
+const maxAge=60;
+
+
+function if_else(age){
+  if (typeof age !== 'number') {
+    console.log( `Incorrect data type, making '${age}' type number.`)
+    Number(age)
+  };
+
+  if (age<minAge){
+    console.log( `You don't have access cause your age is ${age}. It's less then ${minAge}.`)
+  }
+  else if(age>=minAge && age<maxAge){
+    console.log( `Welcome ${age}!`)
+  }
+  else if(age>maxAge){
+    console.log( `Keep calm and look Culture channel`)
+  }
+  else
+    {console.log( `Technical work`)}
+};
+
+if_else('2')
