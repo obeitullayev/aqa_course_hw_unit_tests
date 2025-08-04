eccdb19c70498703cff1f53d2d3ab48f7e0737dc
obeitullaev <beyocman@gmail.com>
2025-08-04 21:48:16 +0300

    solved task2-3

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -4,7 +4,18 @@
  * Ожидаемый результат для `hello`: "hello contains 2 vowels and 3 consonants".
  */
 const word = 'hello';
-
+const vowels = 'aeiou'
 let vowelsAndConsonantsResult = '';
+let vowels_counter=0
+
+for (let i=0; i<word.length; i++){
+    for (let j=0; vowels.length>j; j++){
+        if (word[i]===vowels[j]){
+            vowels_counter++
+        }
+    }
+}
+
+vowelsAndConsonantsResult+=`hello contains ${vowels_counter} vowels and ${word.length-vowels_counter} consonants`
 
 export { vowelsAndConsonantsResult };
