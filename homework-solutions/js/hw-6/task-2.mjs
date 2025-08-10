5456eb9bdf8ebb5c8fbb5e47cc504ae750285448
obeitullaev <beyocman@gmail.com>
2025-08-10 18:18:40 +0300

    added hw-6

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -1,6 +1,6 @@
 /*
   У вас есть массив названий пицц вашего конкурента.
-  Создайте скрипт с циклом, который будет проверять ваш набор названий пицц (массив) 
+  Создайте скрипт с циклом, который будет проверять ваш набор названий пицц (массив)
   и набор названий пицц конкурента (массив), пицц которых нет у конкурента присвойте в переменную "resultUnique" (массив).
   Если все ваши пиццы есть у конкурента результатом будет "null" присвойте в переменную "resultNull".
 
@@ -8,12 +8,31 @@
   Воспользуйтесь наборами пицц, что приведены ниже.
 
   Пиццы:
-  const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
-  const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
-  const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+
 */
 
-let resultUnique;
-let resultNull;
+const resultUnique= [];
+let resultNull=0;
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+const myPizzas =[myPizzasT1, myPizzasT2]
+const lowerCompetitors =[]
+
+for (const _ of competitorPizzas) lowerCompetitors.push(_.toLowerCase())
+
+for (const _ of myPizzas) {
+  for (const Pizza of _){
+    const pizzaLower = Pizza.toLowerCase();
+    if (lowerCompetitors.includes(pizzaLower)){
+      resultNull++
+      }
+    else resultUnique.push(Pizza)
+    }
+
+  if (resultNull===_.length) resultNull = null
+  else resultNull = 0
+
+}
 
 export { resultNull, resultUnique };
