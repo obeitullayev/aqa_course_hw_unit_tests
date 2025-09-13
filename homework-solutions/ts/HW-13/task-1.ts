afb828b11940479a70ce45d10b7a048d8183fdcc
obeitullaev <beyocman@gmail.com>
2025-09-13 17:07:11 +0300

    added HW-13

---
+++ homework-solutions/ts/HW-13/task-1.ts
@@ -0,0 +1,86 @@
+
+// 1. Объявите переменные для каждого из следующих типов данных с явным указанием типа после символв "двоеточие":
+//     - Число: переменная num1, значение 42.
+//     - Строка: переменная str, значение "Hello, TypeScript!".
+//     - Булево: переменная isComplete, значение true.
+//     - Массив чисел: переменная numbers, значения [1, 2, 3, 4, 5].
+//     - Массив строк: переменная cities, значения ["Minsk", "Warsaw", "London"].
+//     - Объект: переменная person, объект с полями name: "Alice", age: 30, city: "Minsk".
+
+let num1: Number = 42;
+let str: String ="Hello, TypeScript!";
+let isComplete: Boolean = true;
+let numbers: Array<Number>=[1, 2, 3, 4, 5];
+let cities: Array<String>=["Minsk", "Warsaw", "London"];
+let person ={
+    name: "Alice",
+    age: 30,
+    city: "Minsk",
+    }
+
+// 2. Создайте псевдонимы типов:
+//     - Тип User, который содержит поля name (строка), age (число), и опциональное поле email (строка).
+//     - Тип Grade, который может принимать одно из значений: 'junior' | 'middle' | 'senior'.
+type User = {
+    name:string,
+    age:number,
+    email?:string,
+}
+type Grade = 'junior' | 'middle' | 'senior'
+
+// 3. Создайте интерфейс для объекта Car, который должен содержать поля:
+//     - brand (строка),
+//     - model (строка),
+//     - опциональное поле year (число).
+
+interface Car {
+    brand: string;
+    model: string;
+    year?: number
+}
+
+// 4. Создайте интерфейсы для:
+//     - Интерфейса Address с полями street (строка), city (строка), и zipCode (число).
+//     - Интерфейса FullAddress, который наследует интерфейс Address и добавляет поле country (строка).
+
+interface Address {
+    street: string;
+    city: string;
+    zipCode: number;
+}
+
+interface FullAddress extends Address {
+    country: string;
+}
+
+// 5. Создайте объединение типов:
+//     - Тип Product с полями id (число), name (строка), и price (число).
+//     - Тип DiscountedProduct, который объединяет Product и добавляет поле discount (число).
+
+type Product = {
+    name:string,
+    id:number,
+    price:number,
+}
+
+type DiscountedProduct = {
+    discount:number,
+} & Product
+
+// 6. Создайте функцию calculateDiscount, которая принимает объект типа DiscountedProduct и возвращает итоговую цену с учетом скидки. 
+//    Затипизировать явно и входные и выходные данные. Используйте следующие данные:
+
+const product: DiscountedProduct = {
+    id: 1,
+    name: "Laptop",
+    price: 1000,
+    discount: 10
+  };
+
+const calculateDiscount = (price:number, discount:number): number =>  {
+    return price-(price*(discount/100))
+}
+
+console.log(calculateDiscount(product.price, product.discount)); // Ожидается: 900
+
+
